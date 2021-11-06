66523505f72740a3bdd3c713b1c87a39a94d9b87
Emil Janik <janikemil@gmail.com>
2021-11-06 11:47:16 +0000

    insert-data.sql created online with Bitbucket

---
+++ src/main/resources/sql/insert-data.sql
@@ -0,0 +1,18 @@
+INSERT INTO user(user_id, user_name, user_surname, user_paassword, email, join_date) VALUES (001, JABBA, HUTT, ABC123, jabba_hutt@gmail.com);
+INSERT INTO follower(follower_id, follower_name, follower_surname, email, start_following) VALUES (001, GREGORY, SAMSUNGOWSKI, gregory_samsungowski, 2021.01.01);
+INSERT INTO post(post_id, creator_name, creator_surname, creator_email, time_of_cration, post_content) VALUES (001, JABBA, HUTT, jabba_hutt@gmail.com, 2021.11.06, "I love Star Wars");
+INSERT INTO user(user_id, user_name, user_surname, user_paassword, email, join_date) VALUES (002, FRIEDERICH, GAUSS, DEF456, friederich_gasus@gmail.com);
+INSERT INTO follower(follower_id, follower_name, follower_surname, email, start_following) VALUES (002, IGOR, STRAVINSKY, igor_stravinsky, 2021.02.02);
+INSERT INTO post(post_id, creator_name, creator_surname, creator_email, time_of_cration, post_content) VALUES (002, FRIEDERICH, GAUSS, friederich_gasus@gmail.com, 2021.11.03, "People overuse the normal distribution.");
+INSERT INTO user(user_id, user_name, user_surname, user_paassword, email, join_date) VALUES (003, THOMAS, SLODOWICZ, QWER123$, Thomas_slodowicz@yahoo.com);
+INSERT INTO follower(follower_id, follower_name, follower_surname, email, start_following) VALUES (003, THOMAS, SLODOWICZ, thomas_slodowicz, 2021.02.03);
+INSERT INTO post(post_id, creator_name, creator_surname, creator_email, time_of_cration, post_content) VALUES (003, THOMAS, SLODOWICZ, Thomas_slodowicz@yahoo.com, 2021.02.03, “I love chocolate cookies!”);
+INSERT INTO user(user_id, user_name, user_surname, user_paassword, email, join_date) VALUES (004, JENNIE, CHARTLOOTE, Asd1234%, jennie_charlotte@o2.com);
+INSERT INTO follower(follower_id, follower_name, follower_surname, email, start_following) VALUES (004, JENNIE, CHARLOTTE, jennie_charlotte, 2021.04.04);
+INSERT INTO post(post_id, creator_name, creator_surname, creator_email, time_of_cration, post_content) VALUES (004, JENNIE, CHARLOTTE, jennie_charlotte@02.com, 2021.04.04, “I love sleeping until evening!”);
+INSERT INTO user(user_id, user_name, user_surname, user_password, email, join_date) VALUES (005, 'JACK', 'JACKSON', 'catsanddogs123', 'jack_jackson@gmail.com', 06.6.2021);
+INSERT INTO follower(follower_id, follower_name, follower_surname, email, start_following) VALUES (005, 'BOB', 'BOBSON', 'bob_bobson@gmail.com', 06.7.2021);
+INSERT INTO post (creator_id, creator_name, creator_surname, creator_email, time_of_creation, post_content) VALUES (005, 'JACK, 'JACKSON', 'jack_jackson@gmail.com', 07.6.2021, 'I graduated!');
+INSERT INTO user(user_id, user_name, user_surname, user_password, email, join_date) VALUES (006, 'JAMES', JAMESON', 'carsandbikes123', 'james_jameson@gmail.com', 07.6.2021);
+INSERT INTO follower(follower_id, follower_name, follower_surname, email, start_following) VALUES (006, 'TOM', 'TOMSON', 'tom_tomson@gmail.com', 07.7.2021);
+INSERT INTO post(creator_id, creator_name, creator_surname, creator_email, time_of_creation, post_content) VALUES (006, 'JAMES, 'JAMESON', 'james_jameson@gmail.com', 08.6.2021, 'I got married today!');
