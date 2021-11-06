437ecc57c279b21d98315d33d3a86230ba420563
Emil Janik <janikemil@gmail.com>
2021-11-06 14:00:36 +0000

    create-db.sql edited online with Bitbucket

--- src/main/resources/sql/create-db.sql
+++ src/main/resources/sql/create-db.sql
@@ -5,16 +5,16 @@ user_id INTEGER NOT NULL PRIMARY KEY,
 user_name VARCHAR(100),
 user_surname VARCHAR(100),
 user_paassword VARCHAR(100),
-email VARCHAR(100),
-join_date TIMESTAMP
+user_email VARCHAR(100),
+join_date VARCHAR(100)
 );
 drop table follower if exists;
 create table follower (
 follower_id INTEGER NOT NULL PRIMARY KEY,
 follower_name VARCHAR(100),
 follower_surname VARCHAR(100),
-email VARCHAR(100),
-start_following TIMESTAMP,
+follower_email VARCHAR(100),
+start_following VARCHAR(100)
 );
 drop table post if exists;
 create table post (
@@ -22,6 +22,6 @@ post_id INTEGER NOT NULL PRIMARY KEY,
 creator_name VARCHAR(100),
 creator_surname VARCHAR(100),
 creator_email VARCHAR(100),
-time_of_cration TIMESTAMP,
+time_of_cration VARCHAR(100),
 post_content VARCHAR(200),
 );
